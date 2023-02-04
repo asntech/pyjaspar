2627df9d61923f9b49eeab2b8de0b37fd891dec3
asntech <azez.khan@gmail.com>
2023-02-04 20:53:30 +0100

    minor updates and fixes

--- pyjaspar/__init__.py
+++ pyjaspar/__init__.py
@@ -42,7 +42,7 @@ Example, substitute the database release/version name::
         for motif in motifs:
             pass # do something with the motif
 """
-__version__ = '2.1.0'
+__version__ = '2.1.1'
 
 
 import warnings
